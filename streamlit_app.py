eafebbfcd26521e6522f625a33bd76e90a754eae
nylzena <naylazena5@gmail.com>
2025-12-11 15:46:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title(":purple[Projek LPK 2025]")
+st.title(":green[Projek LPK 2025]")
 st.header(":blue[Penentuan bilangan dan genap]")
 number = st.number_input("Insert a number",min_value=0, max_value=10000)
 if number%2==1:
