c87ee2664b820b1756f131dfa8caab016b69d468
nylzena <naylazena5@gmail.com>
2025-12-11 15:25:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("MY PROJECT LPK 2025")
+st.title("_NTP_ is :blue[cool] :sunglasses:")
