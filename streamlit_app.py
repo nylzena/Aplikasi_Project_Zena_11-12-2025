f5777692ec54123e4b3cb34206ee095fc19719cc
nylzena <naylazena5@gmail.com>
2025-12-11 15:30:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,18 @@ import streamlit as st
 
 st.title(":purple[MY PROJECT LPK 2025]")
 st.title(":orange[_NTP_] is :blue[cool] :sunglasses:")
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
