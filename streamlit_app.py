5a09fac4f79d76d3d5615d1020acfaa40773d833
nylzena <naylazena5@gmail.com>
2025-12-11 15:43:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title(":purple[Projek LPK 2025]")
 st.title(":blue[Penentuan bilangan dan genap]")
-number = int(st.number_input("Masukkan angka",min_value=0, max_value=10000))
+number = (number_input("Insert a number",min_value=0, max_value=10000))
 if number%2=1:
     st.write("Bilangan",number,"termasuk bilangan ganjil")
 else:
